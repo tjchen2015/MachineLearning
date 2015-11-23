72a824a1913c83e45441908801f15d353e2522fc
Justin Lin <tony199462@gmail.com>
2015-11-23 20:12:17 +0800

    test neural network

--- src/test2.m
+++ src/test2.m
@@ -9,9 +9,10 @@ input = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1;
                  0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1];
              
 output = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-
-[net, tri] = train(net, input, output,nnMATLAB);
+for t = 1:1000
+    [net, tri] = train(net, input, output,nnMATLAB);
+end
 view(net);
 
-test15 = [0;0;-1;0];
-t15 = net(test15);
+t15 = net(input);
+
