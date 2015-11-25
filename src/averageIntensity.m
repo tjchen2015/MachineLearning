c5e33f93ddb0623ccd0b2849331b698dd586da13
tjchen <tzujung2627@gmail.com>
2015-11-25 11:51:36 +0800

    update integral image and average intensity

--- src/averageIntensity.m
+++ src/averageIntensity.m
@@ -7,22 +7,38 @@ function u = averageIntensity(II, x1, y1, x2, y2)
 
 T = (x2-x1+1) * (y2-y1+1);%number of pixels within rectangular region
 
+%check index out of bound
 if x1 < 1
     x1 = 1;
 end
-if x2 > width
-    x2 = width;
-end
-
 if y1 < 1
     y1 = 1;
+end
+
+if x2 > width
+    x2 = width;
 end
 if y2 > height
     y2 = height;
 end
 
-A = II(y1, x1);
-B = II(y1, x2);
-C = II(y2, x1);
+%assign values
+if x1==1 && y1==1
+    A = 0;
+    B = 0;
+    C = 0;
+elseif x1 == 1
+    A = 0;
+    B = II(y1-1, x2);
+    C = 0;
+elseif y1 == 1
+    A = 0;
+    B = 0;
+    C = II(y2, x1-1);
+else
+    A = II(y1-1, x1-1);
+    B = II(y1-1, x2);
+    C = II(y2, x1-1);
+end
 
 u = (II(y2, x2) + A - B - C) / T;
--- src/preTest.m
+++ src/preTest.m
@@ -1,12 +1,21 @@
-clear
-clc
+% A = [1,2,3;4,5,6;1,2,3;4,5,6]
+% II1 = myIntegralImage(A.^2)
+% II = myIntegralImage(A);
+% II2 = II.^2
+% u = averageIntensity(II,2,2,3,4)
 
 testImage = imread('D:\HomeworkWorkspace\Machine Learning\Final project\UBIRIS\1.bmp');
-testImage = double(testImage);
-
-scFeatureSet = scleraFeatureSet(testImage, 1);
-scFeatureSet{480,638}
-
-detectedSclera = imread('D:\HomeworkWorkspace\Machine Learning\answer\ansSclera1.bmp');
-detectedSclera = double(detectedSclera);
-irisFeatureSet = irisFeatureSet(testImage, detectedSclera, 1);
+testImage = double(testImage(1:3, 1:4, :))
+imageHSV = rgb2hsv(testImage);
+    imageH = imageHSV(:, :, 1)
+    imageYCbCr = rgb2ycbcr(testImage);
+    imageCb = imageYCbCr(:, :, 2)
+    imageCr = imageYCbCr(:, :, 3)
+% mine = myIntegralImage(testImage);
+% 
+% scFeatureSet = scleraFeatureSet(testImage, 1);
+% scFeatureSet{480,638}
+% 
+% detectedSclera = imread('D:\HomeworkWorkspace\Machine Learning\answer\ansSclera1.bmp');
+% detectedSclera = double(detectedSclera);
+% irisFeatureSet = irisFeatureSet(testImage, detectedSclera, 1);
