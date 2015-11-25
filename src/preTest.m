7b48cec7035d020084fa43c1895b601d69f9a6b4
tjchen <tzujung2627@gmail.com>
2015-11-25 21:44:28 +0800

    update

--- src/preTest.m
+++ src/preTest.m
@@ -1,17 +1,3 @@
-% A = [1,2,3;4,5,6;1,2,3;4,5,6]
-% II1 = myIntegralImage(A.^2)
-% II = myIntegralImage(A);
-% II2 = II.^2
-% u = averageIntensity(II,2,2,3,4)
-% imageHSV = rgb2hsv(testImage(1:3, 1:4, :));
-% for i=1:3
-%     disp([min(min(imageHSV(:,:,i))), max(max(imageHSV(:,:,i)))]);
-% end
-% imageH = imageHSV(:, :, 2)
-% imageYCbCr = rgb2ycbcr(testImage(1:3, 1:4, :));
-% imageCb = imageYCbCr(:, :, 2)
-% imageCr = imageYCbCr(:, :, 3)
-
 testImage = imread('D:\HomeworkWorkspace\Machine Learning\Final project\ICE\1.bmp');
 testImage = double(testImage);
 scFeatureSet = scleraFeatureSet(testImage, 1);
