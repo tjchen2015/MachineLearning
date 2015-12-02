b093d7f5bf7ef529cd15145c3c908dc4bbaf57a6
Justin Lin <tony199462@gmail.com>
2015-12-02 22:56:01 +0800

    Clean repository

--- src/test2.m
+++ src/test2.m
@@ -1,23 +1,45 @@
 clear
 clc
 
-% load('f.mat');
-% load('net.mat');
-% 
-% ans = net(f);
-% ans = reshape(ans, 480, 640);
 
-load('a1000.mat');
-figure;
-imagesc(a);
-colormap gray;
+scleraFeature = [];%sclera feature sets of each image in matrix form
+scleraAnswer = [];%expected result of neural network
+r = 480;
+c = 640;
+net = feedforwardnet([12, 12*1.5], 'traincgf');
 
-load('a10.mat');
-figure;
-imagesc(a);
-colormap gray;
+%train 1 ~ 7
+for n = 1:7
+    n
+    filename = ['./scleraFeatureSet/scNew', int2str(n)];
+    feature = load(filename);
+    feature = feature.scFeatureSet;
+    scleraFeature = horzcat(scleraFeature, feature);
 
-% imFile = strcat('ICE/', 'ansSclera1.bmp');
-% scleraImage = imread(imFile);
-% figure;
-% imshow(scleraImage);
+    filename = ['C:\Users\Justin\Dropbox\Data Science\MachineLearning\NCU course\ICE\answer\ansSclera', int2str(n), '.bmp'];
+    scleraImage = imread(filename);
+    scleraImage = double(scleraImage) / 255;
+    scleraImage = reshape(scleraImage.', 1, r*c);
+    scleraAnswer = horzcat(scleraAnswer, scleraImage);
+    
+    net = train(net, scleraFeature, scleraAnswer);
+    
+end
+
+
+%test 8
+for b = 8:10
+    filename =[ './scleraFeatureSet/scNew', int2str(n)];
+    feature = load(filename);
+    feature = feature.scFeatureSet;
+    
+    a = net(feature);
+    a = reshape(a, c, r);
+    a = a';
+    
+    imagesc(a);
+    colormap gray;
+end
+        
+
+
