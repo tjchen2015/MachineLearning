7f4ec60ccf9ac471ca3d2cd807b8b40ceae8d692
tjchen <tzujung2627@gmail.com>
2015-12-17 23:46:42 +0800

    update avgIntensity, stdDeviation, sclera to speed up

--- src/averageIntensity.m
+++ src/averageIntensity.m
@@ -1,43 +1,6 @@
-function u = averageIntensity(II, x1, y1, x2, y2)
+function u = averageIntensity(subImage)
 %input: integral image II, rectangular region
     %upper-left (x 1, y1) bottom-right (x2, y2)
 %output: average intensity u within rectangular region
 
-[height, width] = size(II);
-
-%check index out of bound
-if x1 < 1
-    x1 = 1;
-end
-if y1 < 1
-    y1 = 1;
-end
-
-if x2 > width
-    x2 = width;
-end
-if y2 > height
-    y2 = height;
-end
-
-%assign values
-if x1==1 && y1==1
-    A = 0;
-    B = 0;
-    C = 0;
-elseif x1 == 1
-    A = 0;
-    B = II(y1-1, x2);
-    C = 0;
-elseif y1 == 1
-    A = 0;
-    B = 0;
-    C = II(y2, x1-1);
-else
-    A = II(y1-1, x1-1);
-    B = II(y1-1, x2);
-    C = II(y2, x1-1);
-end
-
-T = (x2-x1+1) * (y2-y1+1);%number of pixels within rectangular region
-u = (II(y2, x2) + A - B - C) / T;
+u = sum(subImage) / numel(subImage);
--- src/testFeatureSet.m
+++ src/testFeatureSet.m
@@ -1,11 +1,13 @@
 % function [] = testFeatureSet()
 
-for i = 1:10
-    testImage = imread(sprintf('D:\\HomeworkWorkspace\\Machine Learning\\Final project\\databases\\UBIRIS\\%d.bmp', i));
+for i = 1:1
+    tic
+    testImage = imread(sprintf('D:\\HomeworkWorkspace\\Machine Learning\\Final project\\databases\\ICE\\%d.bmp', i));
     scFeatureSet = scleraFeatureSet(double(testImage));
     
-    filename = ['D:\HomeworkWorkspace\Machine Learning\Final project\data\feature sets\UBIRIS\sc' num2str(i)];
+    filename = ['D:\HomeworkWorkspace\Machine Learning\Final project\data\feature sets\ICE\sc_' num2str(i)];
     save(filename, 'scFeatureSet');
+    toc
 end
 
 % detectedSclera = imread(sprintf('D:\\HomeworkWorkspace\\Machine Learning\\Final project\\databases\\answer\ansSclera1.bmp');
