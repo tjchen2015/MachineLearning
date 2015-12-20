5de9288db5488a9e147a92be7192777887bc5fc6
tjchen <tzujung2627@gmail.com>
2015-12-21 00:28:40 +0800

    update testing code

--- src/testFeatureSet.m
+++ src/testFeatureSet.m
@@ -1,13 +1,11 @@
 % function [] = testFeatureSet()
 
-for i = 1:1
-    tic
+for i = 1:30
     testImage = imread(sprintf('D:\\HomeworkWorkspace\\Machine Learning\\Final project\\databases\\ICE\\%d.bmp', i));
     scFeatureSet = scleraFeatureSet(im2double(testImage));
     
-    filename = ['D:\HomeworkWorkspace\Machine Learning\Final project\data\feature sets\ICE\sc_' num2str(i)];
+    filename = ['D:\HomeworkWorkspace\Machine Learning\Final project\data\feature sets\ICE\sc' num2str(i)];
     save(filename, 'scFeatureSet');
-    toc
 end
 
 % detectedSclera = imread(sprintf('D:\\HomeworkWorkspace\\Machine Learning\\Final project\\databases\\answer\ansSclera1.bmp');
