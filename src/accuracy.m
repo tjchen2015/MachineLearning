bf9ef664e894dcfef7713053ce62c515773ef850
Justin Lin <tony199462@gmail.com>
2015-12-30 18:17:11 +0800

    little fix

--- src/accuracy.m
+++ src/accuracy.m
@@ -1,6 +1,6 @@
 function correct = accuracy( ourAns, teacherAns )
 %ACCURACY computes accuracy of our segmentation result. The formula is
-%define as Accuracy = Error^2/Max_error^2. Error = norm(ourAns -
+%define as Accuracy = 1 - Error^2/Max_error^2. Error = norm(ourAns -
 %teacherAns). Max_error^2 = e1 + e2 + e3 + e4. e1 = e3 = (n^2 + m^2)/4. n is #
 %of row, m is # of column. e2 = r_hat/4. r_hat is the expected distance of inner
 %boundary. e4 = R_hat/4. R_hat is the expected distance of outer boundary. 
@@ -11,6 +11,7 @@ function correct = accuracy( ourAns, teacherAns )
 ourAns = round(ourAns);
 error = ourAns - teacherAns;
 error = error.^2;
+error = sum(error);
 
 %determine max square error for two center points
 scaleP = 0.5;
@@ -26,7 +27,7 @@ maxe4 = scaleR.^2.*teacherAns(6);
 
 %sum up all max error to define error rate
 maxError = maxe1e3+maxe2+maxe4;
-error = sum(error)/maxError;
+error = error/maxError;
 
 % if error over 100% then set it as 100%
 if error>1
