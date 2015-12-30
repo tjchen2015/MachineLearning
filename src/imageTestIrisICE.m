5321cda73d9a76f348c6432e0f95b7ecbc97652f
ML <ML@testarea.com>
2015-12-30 19:15:33 +0800

    add test result code

---
+++ src/imageTestIrisICE.m
@@ -0,0 +1,22 @@
+net = load('../output/neural network/ICE/iris/2Layer_40Image_train10');
+net = net.net;
+
+imageID = 140;
+testImageFile = sprintf('../databases/ICE/%d.bmp', imageID);
+testImage = imread(testImageFile);
+testImage = im2double(testImage);
+detectedScleraFile = sprintf('../answer/ICE/ansSclera%d.bmp', imageID);
+detectedSclera = imread(detectedScleraFile);
+detectedSclera = im2double(detectedSclera);
+
+irisFeatureSet = irisFeatureSet(testImage, detectedSclera);
+
+output = net(irisFeatureSet);
+output1 = reshape(output, 640, 480).';
+% output2 = im2bw(output, 0.7);%binarization
+% output2 = reshape(output2, 640, 480).';
+imagesc(output1);
+colormap gray;
+
+filename1 = ['../output/image/ICE/iris' num2str(imageID) '.bmp'];
+imwrite(output1, filename1);
