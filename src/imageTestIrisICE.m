34127e66824a73049bff902e616eefe4ec9cabfe
ML3 <ML3@testarea.com>
2015-12-31 10:51:58 +0800

    modify variable name, file

--- src/imageTestIrisICE.m
+++ src/imageTestIrisICE.m
@@ -1,17 +1,17 @@
-net = load('../output/neural network/ICE/2Layer_40Image_train10');
+net = load('../output/neural network/ICE/iris/2Layer_40Image_train10');
 net = net.net;
 
-imageID = 14;
+imageID = 35;
 testImageFile = sprintf('../databases/ICE/%d.bmp', imageID);
 testImage = imread(testImageFile);
 testImage = im2double(testImage);
-detectedScleraFile = sprintf('../answer/ICE/ansIris%d.bmp', imageID);
+detectedScleraFile = sprintf('../answer/ICE/ansSclera%d.bmp', imageID);
 detectedSclera = imread(detectedScleraFile);
 detectedSclera = im2double(detectedSclera);
 
-irisFeatureSet = irisFeatureSet(testImage, detectedSclera);
+irisFeature = irisFeatureSet(testImage, detectedSclera);
 
-output = net(irisFeatureSet);
+output = net(irisFeature);
 output1 = reshape(output, 640, 480).';
 % output2 = im2bw(output, 0.7);%binarization
 % output2 = reshape(output2, 640, 480).';
