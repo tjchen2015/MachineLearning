bbc56ee0a3c808d006f6e802caec6ac86294e311
ML <ML@testarea.com>
2016-01-02 00:51:19 +0800

    add test result code

---
+++ src/imageTestIrisUBIRIS.m
@@ -0,0 +1,24 @@
+net = load('../output/neural network/UBIRIS/iris/2Layer_40Image_train10');
+net = net.net;
+
+for imageID=[1 10 11 12 13 14 15 16 17 18 19 20]
+%     imageID = 140;
+    testImageFile = sprintf('../answer/UBIRIS/%d/img.jpg', imageID);
+    testImage = imread(testImageFile);
+    testImage = im2double(testImage);
+    detectedScleraFile = sprintf('../answer/UBIRIS/%d/sclera_mask.bmp', imageID);
+    detectedSclera = imread(detectedScleraFile);
+    detectedSclera = im2double(detectedSclera);
+
+    irisFeatureSet = irisFeatureSet(testImage, detectedSclera);
+
+    output = net(irisFeatureSet);
+    output1 = reshape(output, 800, 600).';
+    % output2 = im2bw(output, 0.7);%binarization
+    % output2 = reshape(output2, 800, 600).';
+    imagesc(output1);
+    colormap gray;
+
+    filename1 = ['../output/image/UBIRIS/iris' num2str(imageID) '.bmp'];
+    imwrite(output1, filename1);
+end
---
+++ src/imageTestScleraUBIRIS.m
@@ -0,0 +1,21 @@
+net = load('../output/neural network/UBIRIS/sclera/2Layer_40Image_train10');
+net = net.net;
+
+for imageID=[1 10 11 12 13 14 15 16 17 18 19 20]
+%     imageID = 140;
+    filename = sprintf('../answer/UBIRIS/%d/img.jpg', imageID);
+    testImage = imread(filename);
+    testImage = im2double(testImage);
+
+    scFeatureSet = scleraFeatureSet(testImage);
+
+    output = net(scFeatureSet);
+    output1 = reshape(output, 800, 600).';
+    % output2 = im2bw(output, 0.7);%binarization
+    % output2 = reshape(output2, 800, 600).';
+    imagesc(output1);
+    colormap gray;
+
+    filename1 = ['../output/image/UBIRIS/sclera' num2str(imageID) '.bmp'];
+    imwrite(output1, filename1);
+end
