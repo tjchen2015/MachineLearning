e3de1f252326a47b8b6ea61e62a8bba0024c3e0c
tjchen <tzujung2627@gmail.com>
2016-01-10 11:20:33 +0800

    modify code to get accuracyMat from files

--- src/+Testing/ICE_CalAccuracy.m
+++ src/+Testing/ICE_CalAccuracy.m
@@ -1,11 +1,15 @@
 allAccuracy = [];
 partAccuracy = load('../output/ICEAccuracy_1to750.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/ICEAccuracy_751to1500.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/ICEAccuracy_1501to2250.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/ICEAccuracy_2251to2953.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 
 avgAccuracy = mean(allAccuracy)
--- src/+Testing/UBIRIS_CalAccuracy.m
+++ src/+Testing/UBIRIS_CalAccuracy.m
@@ -1,11 +1,15 @@
 allAccuracy = [];
 partAccuracy = load('../output/UBIRISAccuracy_1to300.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/UBIRISAccuracy_301to600.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/UBIRISAccuracy_601to900.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 partAccuracy = load('../output/UBIRISAccuracy_901to1203.mat');
+partAccuracy = partAccuracy.accuracyMat;
 allAccuracy = [allAccuracy; partAccuracy];
 
 avgAccuracy = mean(allAccuracy)
